9770d8cd52c549c4ba59cb37b0df875a2ce69268
freesgen <jesusant.guerrero@gmail.com>
2020-05-14 03:15:33 -0400

    Complete basics of xstate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,104 +1,62 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  
+    <div v-if="quarter">
+      <div v-for="quarterState in Object.keys(states)" :key="quarterState" :class="{selected: quarterState == state}">
+        {{ quarterState }}
+      </div>
+    </div>
+    <button @click="toggleState">Toggle State</button>
   </div>
 </template>
 
 <script>
+import { createMachine, interpret } from "xstate";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      quarter: null,
+      state: null,
+      states: {
+        do: {
+          on: { TOGGLE: "delegate" }
+        },
+        delegate: {
+          on: { TOGGLE: "schedule" }
+        },
+        schedule: {
+          on: { TOGGLE: "drop" }
+        },
+        drop: {
+          on: { TOGGLE: "do" }
+        }
+      }
+    };
+  },
+  mounted() {
+    const fourQuarterMachine = createMachine({
+      id: "quarter",
+      initial: "do",
+      states: this.states
+    });
+
+    const quarterService = interpret(fourQuarterMachine)
+      .onTransition(state => {
+        this.state = state.value;
+      })
+      .start();
+    this.quarter = quarterService;
+  },
+  methods: {
+    toggleState() {
+      this.quarter.send("TOGGLE");
+    }
   }
 };
 </script>
@@ -118,5 +76,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.selected {
+  color: dodgerblue;
 }
 </style>
