92bbb26ed439feb5675c416d5f2cfd9fd94a60bd
freesgen <jesusant.guerrero@gmail.com>
2020-05-30 17:08:51 -0400

    fix login machine

--- src/components/quadrant/index.vue
+++ src/components/quadrant/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <layout
-      :user="quadrantApp && quadrantApp.state.context.user"
+      :user="state.context && state.context.user"
       :app-machine="quadrantApp"
     ></layout>
     <div class="container-fluid text-primary">
@@ -13,7 +13,7 @@
       </div>
       <div class="text-primary container-fluid">
         <div id="auth-container" v-if="matches('unauthenticated')"></div>
-        {{ quadrantApp && quadrantApp.state.value }}
+        {{ state.value }} {{ state.context }}
       </div>
     </div>
   </div>
@@ -24,32 +24,93 @@ import { interpret } from "xstate";
 import appMachine from "./appMachine";
 import Layout from "@/components/quadrant/layout";
 
+import * as firebase from "firebase";
+import * as firebaseui from "firebaseui";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: "ic-quadrant.firebaseapp.com",
+  databaseURL: "https://ic-quadrant.firebaseio.com",
+  projectId: "ic-quadrant",
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE,
+  messagingSenderId: process.env.VUE_APP_MESSAGE_SENDER,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: "G-VFBCE4GTYC"
+};
+
+firebase.initializeApp(firebaseConfig);
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+const uiConfig = {
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ]
+};
 export default {
   components: {
     Layout
   },
   data() {
     return {
-      quadrantApp: null
+      quadrantApp: null,
+      state: {}
     };
+  },
+  watch: {
+    "state.value": {
+      handler(state) {
+        if (state == 'unauthenticated') {
+          this.initFirebaseAuth();
+        }
+      },
+      immediate: true
+    }
   },
   mounted() {
     this.initMachine();
   },
   methods: {
     initMachine() {
-      this.quadrantApp = appMachine;
-      this.quadrantApp.start();
+      const quadrantAppService = interpret(appMachine)
+        .onTransition(state => {
+          this.state = state;
+        })
+        .start();
+
+      this.quadrantApp = quadrantAppService;
       this.quadrantApp.send("FETCH_TOKEN");
     },
+
     matches(stateName) {
       return this.quadrantApp && this.quadrantApp.state.matches(stateName);
     },
+
     send(stateName, payload) {
       return (
         this.quadrantApp &&
         this.quadrantApp.send({ type: stateName, payload: payload })
       );
+    },
+
+    initFirebaseAuth() {
+      setTimeout(() => {
+        try {
+          ui.start("#auth-container", {
+            signInOptions: uiConfig.signInOptions,
+            siteName: "Vueground",
+            callbacks: {
+              signInSuccessWithAuthResult: result => {
+                const user = result.additionalUserInfo.profile;
+                this.send("SET_USER", user);
+                this.send("LOGIN");
+                return false;
+              }
+            },
+            signInFlow: "popup",
+            signInSuccessUrl: "/quadrant"
+          });
+        } catch (e) {}
+      }, 1000);
     }
   }
 };
